65cc72fddd4486e212479c983e5b2eff6932bcac
Xtr3me2K <130668153+Xtr3me2K@users.noreply.github.com>
2023-05-12 14:17:39 -0300

    Initial commit

---
+++ pagina1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Este documento é tudo, menos uma aula de PHP, sim ou não?</title>
+</head>
+<body>
+    <?php
+
+          echo "<p>Boa tarde a todos!</p>"
+    ?>
+</body>
+</html>
